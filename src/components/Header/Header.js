efa188c8e29059c092138e8c8b64c5e42a9f7ba0
BilalHassen <bilalhassen097@gmail.com>
2023-12-02 10:17:07 -0500

    add in default font size and icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,34 @@
 import "./Header.scss";
 import { NavLink, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useState, useRef } from "react";
 function Header() {
+  const menuRef = useRef();
+  // state to track if the menu is open or not
+  const [open, setOpen] = useState(false);
+
+  const menuToggle = () => {
+    setOpen(!open);
+    menuRef.current.classList.toggle("nav-active");
+  };
+
   return (
     <>
       <div className="header">
         <h1 className="header__title">
           <span>STIFF</span>JAB
         </h1>
+        <button className="header__nav-menu-button" onClick={menuToggle}>
+          <FontAwesomeIcon
+            icon={faBars}
+            fade
+            style={{ color: "#fa0000" }}
+            size="2x"
+          />
+        </button>
         <nav className="header__nav">
-          <ul className="header__nav-list">
+          <ul className="header__nav-list" ref={menuRef}>
             <NavLink>
               <li className="header__nav-list-item">About</li>
             </NavLink>
