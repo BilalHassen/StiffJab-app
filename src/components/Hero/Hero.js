7d7d1f04a9d7afb88c1714cb4acc7b86e1c704d5
BilalHassen <bilalhassen097@gmail.com>
2023-12-03 17:51:14 -0500

    add tablet styling to hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,6 +13,32 @@ function Hero(props) {
           <p className="hero__para-2">and join the strong side!</p>
           <button className="hero__button">Learn More</button>
         </div>
+        <div className="hero__icon-container">
+          <div className="hero__icon-1">
+            <img
+              className="hero__icon-gloves"
+              src={props.glovesIcon}
+              alt="boxing gloves icon"
+            ></img>
+            <p className="hero__icon-p">sparring</p>
+          </div>
+          <div className="hero__icon-2">
+            <img
+              className="hero__icon-bag"
+              src={props.boxingBag}
+              alt="boxing bag icon"
+            ></img>
+            <p className="hero__icon-p">heavy-bag</p>
+          </div>
+          <div className="hero__icon-3">
+            <img
+              className="hero__icon-dumbell"
+              src={props.dumbellIcon}
+              alt="dumbell icon"
+            ></img>
+            <p className="hero__icon-p">strength</p>
+          </div>
+        </div>
       </section>
     </>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,19 @@
 import "./HomePage.scss";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
-import heroImage from "../assets/images/imgbin_mike-tyson-boxing-png.png";
+import glovesIcon from "../assets/images/boxing-gloves.png";
+import boxingBagIcon from "../assets/images/boxing.png";
+import dumbellIcon from "../assets/images/weight.png";
 function HomePage() {
   return (
     <>
       <div className="page__container">
         <Header />
-        <Hero heroImage={heroImage} />
+        <Hero
+          glovesIcon={glovesIcon}
+          boxingBag={boxingBagIcon}
+          dumbellIcon={dumbellIcon}
+        />
       </div>
     </>
   );
