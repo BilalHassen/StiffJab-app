cdcb283215deb87678bbbf6df058004b998e4255
BilalHassen <bilalhassen097@gmail.com>
2023-12-05 12:19:17 -0500

    removed icons from images folder

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,21 @@ function Footer() {
   return (
     <>
       <footer className="footer">
-        <h1 className="footer__title">StiffJab</h1>
+        <h1 className="footer__title">
+          <span className="footer__span">STIFF</span>JAB
+        </h1>
         <p className="footer__para">
           Join us today and become the best version of yourself.
         </p>
+        <div className="footer__socials-container">
+          <div className="footer__icon-container">
+            <img
+              className="footer__instagram"
+              //   src={instaIcon}
+              alt="instagram icon"
+            ></img>
+          </div>
+        </div>
       </footer>
     </>
   );
