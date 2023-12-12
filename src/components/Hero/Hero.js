b05e094959fce4d753eae14962e80d01805e32bf
BilalHassen <bilalhassen097@gmail.com>
2023-12-12 15:47:54 -0500

    change hero image size and added link to opponent match page for mobile

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,16 +1,16 @@
 import "./Hero.scss";
-
+import { Link } from "react-router-dom";
 function Hero(props) {
   return (
     <>
       <section className="hero">
         <div className="hero__text">
           <h1 className="hero__title">Elevate Your skills</h1>
-          <p className="hero__para">
-            Ignite Your Passion and Embrace The Journey to Peak Performance
-          </p>
+          <p className="hero__para">Find you next sparring partner now!</p>
 
-          <button className="hero__button">Learn More</button>
+          <Link to="/opponent-match">
+            <button className="hero__button">Learn More</button>
+          </Link>
         </div>
       </section>
     </>
